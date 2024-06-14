2398482ac6366854be0f311209ed06606a79f49d
* <61941069+Git-Fal7@users.noreply.github.com>
2024-06-14 16:30:23 +0300

    remove old config options

--- config/config.go
+++ config/config.go
@@ -1,11 +1,7 @@
 package config
 
 import (
-	"encoding/base64"
 	"encoding/json"
-	"image"
-	_ "image/png"
-	"log"
 	"os"
 )
 
@@ -17,14 +13,10 @@ type BufferConfig struct {
 
 type Config struct {
 	ListenAddress string       `json:"listen_address"`
-	MaxPlayers    int          `json:"max_players"`
-	Motd          string       `json:"motd"`
-	Restricted    bool         `json:"restricted"`
 	Logs          bool         `json:"logs"`
 	Compression   bool         `json:"enable_compression"`
 	Threshold     int          `json:"compression_threshold"`
 	BufferConfig  BufferConfig `json:"buffer_config"`
-	Favicon       string
 }
 
 var (
@@ -32,26 +24,6 @@ var (
 )
 
 func InitConfig() (err error) {
-	imgFile, _ := os.Open("./favicon.png")
-	defer imgFile.Close()
-
-	img, _, err := image.Decode(imgFile)
-
-	if err == nil {
-		b := img.Bounds()
-
-		if err == nil {
-			if b.Max.X != 64 || b.Max.Y != 64 {
-				log.Printf("Invalid icon for server (resize to 64x64 pixels?)")
-			}
-
-			fav, err := os.ReadFile("./favicon.png")
-			if err == nil {
-				TyphoonConfig.Favicon = "data:image/png;base64," + base64.StdEncoding.EncodeToString(fav)
-			}
-		}
-	}
-
 	file, err := os.ReadFile("./config.json")
 	if err != nil {
 		panic(err)

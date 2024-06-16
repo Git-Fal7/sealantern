2fec73d23d7aeeeaec62b5836a446e30b1d468c4
* <61941069+Git-Fal7@users.noreply.github.com>
2024-06-16 17:25:40 +0300

    Update slot.go

--- pkg/slot/slot.go
+++ pkg/slot/slot.go
@@ -8,3 +8,28 @@ type SlotItem struct {
 	Durability uint16
 	NBT        nbt.Compound
 }
+
+func (item SlotItem) DisplayName() string {
+	displayNBT, ok := item.NBT["display"].(nbt.Compound)
+	if !ok || displayNBT == nil {
+		return ""
+	}
+	displayName, ok := displayNBT["Name"].(nbt.String)
+	if !ok {
+		return ""
+	}
+	return string(displayName)
+}
+
+func (item *SlotItem) SetDisplayName(displayName string) {
+	displayNBT, ok := item.NBT["display"].(nbt.Compound)
+	if !ok || displayNBT == nil {
+		displayNBT = nbt.Compound{}
+	}
+	displayNBT["Name"] = nbt.String(displayName)
+	item.NBT["display"] = displayNBT
+}
+
+func (item *SlotItem) SetCustomNBT(key string, tag nbt.Tag) {
+	item.NBT[nbt.String(key)] = tag
+}

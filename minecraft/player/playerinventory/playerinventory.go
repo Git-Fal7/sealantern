f90033447f089802b10c0dbc990ce83b2b1bb285
* <61941069+Git-Fal7@users.noreply.github.com>
2024-07-01 16:00:53 +0300

    Update playerinventory.go

--- minecraft/player/playerinventory/playerinventory.go
+++ minecraft/player/playerinventory/playerinventory.go
@@ -105,7 +105,7 @@ func (inv *PlayerInventory) AddItem(item slot.SlotItem) {
 	// Look for existing stacks
 	toAdd := int(item.Amount)
 	for i := 0; i < 36 && toAdd > 0; i++ {
-		var selectedSlotIndex = i - 9
+		var selectedSlotIndex = i
 		if i < 9 {
 			selectedSlotIndex = 36 + i
 		}
@@ -129,7 +129,7 @@ func (inv *PlayerInventory) AddItem(item slot.SlotItem) {
 	// Look for empty slots
 	if toAdd > 0 {
 		for i := 0; i < 36 && toAdd > 0; i++ {
-			var selectedSlotIndex = i - 9
+			var selectedSlotIndex = i
 			if i < 9 {
 				selectedSlotIndex = 36 + i
 			}
